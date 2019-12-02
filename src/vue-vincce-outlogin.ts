49d761c5b74ab431f2438b24134e6c468f6f4c29
Ted Yuen <tedyuen.goo@gmail.com>
2019-12-02 19:01:23 +0800

    [build] 0.1.0

--- src/vue-vincce-outlogin.ts
+++ src/vue-vincce-outlogin.ts
@@ -1,6 +1,61 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// import "core-js/fn/array.find"
+// ...
+export class OutLoginUtils {
+  window: any
+  urlSuffix: string
+  currentSysId: string = "0"
+  sysList: Array<any> = []
+
+  constructor(window: any, urlSuffix: string, currentSysId?:string) {
+    this.window = window;
+    this.urlSuffix = urlSuffix;
+    if(currentSysId) {
+      this.currentSysId = currentSysId;
+    }
+  }
+
+  addData(sysList: Array<any>) {
+    this.sysList = sysList
+  }
+
+  setCurrenntSysId(currentSysId:string) {
+    this.currentSysId = currentSysId;
+  }
+
+  renderIframe(options: any) {
+    const { username, password, sysList, currentSysId } = options;
+    this.currentSysId = currentSysId;
+    this.sysList = sysList;
+    let self = this;
+    this.window.addEventListener("message", function (event: any) {
+      console.log('receiveMessageFromIframePage', event)
+      self.window.document.getElementById("idFrame").contentWindow.postMessage({
+        username, password, currentSysId
+      }, '*')
+    }, false);
+    for (let item of this.sysList) {
+      let div = this.window.document.createElement("div");
+      div.innerHTML = '<iframe id="idFrame" name="idFrame" src="http://localhost:8001/cosco-agency-admin/user/outlogin" height = "0" width = "0" frameborder="0" scrolling="auto" style = "display:none;visibility:hidden" ></iframe>';
+      this.window.document.body.appendChild(div);
+    }
+  }
+
 
 }
+
+
+let Plugin = {
+  install: function (Vue: any, options: any) {
+    if (options) {
+      const { window, sysList, urlSuffix } = options;
+      if (window && sysList && urlSuffix) {
+        let outLogin = new OutLoginUtils(options.window, urlSuffix)
+        Vue.ologin = outLogin
+        Vue.prototype.$ologin = outLogin
+      }
+    }
+  }
+}
+
+export default Plugin
